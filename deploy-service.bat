6fba36908109defb19b7be28ed39f17d5f1bf66f
greg Sochanik <greg@sochanik.com>
2013-06-25 17:08:27 +0100

    Added mkdir command

--- deploy-service.bat
+++ deploy-service.bat
@@ -3,4 +3,6 @@ SET env=%2
 SET login=sshduser
 SET webroot=C:\inetpub\%3\
 
+c:\TeamCityBuildTools\PLINK -i c:\TeamCityBuildTools\DeploymentKeyConfig\Key.ppk -P 22 %login%@%target%:%webroot% md %webroot%%env%_deploy | echo 0
+
 c:\TeamCityBuildTools\pscp\pscp -i c:\TeamCityBuildTools\DeploymentKeyConfig\Key.ppk -P 22 -r sitefiles/*.* %login%@%target%:%webroot%%env%_deploy
