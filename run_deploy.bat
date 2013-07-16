9533a8bd78e36132762d63a6e393917b728d02f7
greg Sochanik <greg@sochanik.com>
2013-07-16 11:52:22 +0100

    Attempt to allow chomod 744 to enable batch file to be run oin cygwin

--- run_deploy.bat
+++ run_deploy.bat
@@ -19,6 +19,7 @@ echo cd /C/Users/sshduser > temp-put.bat
 echo mkdir %APP% >> temp-put.bat
 echo cd %APP% >> temp-put.bat
 echo mput temp.bat >> temp-put.bat
+echo chmod 744 temp.bat >> temp-put.bat
 echo cd /%WWWROOT% >> temp-put.bat
 echo mkdir /%WEBROOT% >> temp-put.bat
 echo cd /%WEBROOT%/ >> temp-put.bat
