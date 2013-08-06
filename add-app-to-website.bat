3ebb1c69ab5bf5a0a37a4f29f0fab10b0254d9e2
greg Sochanik <greg@sochanik.com>
2013-08-06 18:17:45 +0100

    ever wished you hadnt started something

--- add-app-to-website.bat
+++ add-app-to-website.bat
@@ -5,13 +5,12 @@ REM website named %HOST% and app pool named %HOST% must already exist - %VIRTUAL
 REM 1. add app to %HOST%
 
 IF NOT "%WEBSITE_SET%"=="true" (
-	SET VIRTUAL_APP_ROOT=%NEW_FOLDER%
+	SET VIRTUAL_APP_ROOT=%WINROOT%%NEW_FOLDER%
 )
 
-%appcmd% add app /site.name:%HOST% /path:%VIRTUAL_APP% /physicalPath:%VIRTUAL_APP_ROOT%
+%appcmd% delete app "%HOST%%VIRTUAL_APP%"
 
-REM 2. set physicalpath for app to %VIRTUAL_APP_ROOT%
-%appcmd% set app "%HOST%%VIRTUAL_APP%" /physicalPath:%VIRTUAL_APP_ROOT%
+%appcmd% add app /site.name:%HOST% /path:%VIRTUAL_APP% /physicalPath:%VIRTUAL_APP_ROOT%
 
 REM 2. set apppool for app to %HOST%
 %appcmd% set app "%HOST%%VIRTUAL_APP%" /applicationPool:%HOST% 
