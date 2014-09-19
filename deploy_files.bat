aebe610ac844eccad927b83f00f46448664773b7
greg Sochanik <greg@sochanik.com>
2014-09-19 16:59:05 +0100

    Try again with wildcards

--- deploy_files.bat
+++ deploy_files.bat
@@ -19,7 +19,7 @@ echo mkdir %ENV_NAME% >> temp-put.bat
 echo mkdir %ENV_NAME%_deploy >> temp-put.bat
 echo chmod 755 -R * >> temp-put.bat
 echo cd %ENV_NAME%_deploy >> temp-put.bat
-echo rm -fr ** >> temp-put.bat
+echo rm ** >> temp-put.bat
 echo mput sitefiles/*.* >> temp-put.bat
 
 REM - Put all files on server
